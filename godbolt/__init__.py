6a0755f56c18b1e6412bb0a193181930d6944b0f
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-30 12:56:33 +0530

    Update __init__.py

--- godbolt/__init__.py
+++ godbolt/__init__.py
@@ -1,7 +1,23 @@
-from typing import Dict
+from .models import *
+from typing import Dict, List, Mapping
 
 
 class Godbolt:
   def __init__(self, headers: Dict[str, str] = {}) -> None:
     headers['Accept'] = "application/json"
     self.__headers: Dict[str, str] = headers
+    self.__languages: List[Language] = []
+
+  def init(self,) -> None:
+    languages = Route('GET', "/languages", headers=headers).request()
+    for language in languages:
+      self.__languages.append(Language.from_dict(language))
+
+  @property
+  def languages(self) -> List[Language]:
+    return self.__languages.copy()
+
+  @property
+  def headers(self) -> Mapping[str, str]:
+    return types.MappingProxyType(self.__headers)
+    
