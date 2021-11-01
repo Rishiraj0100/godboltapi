9f4d839565b8f95faa87d4dedd94c5e79a502076
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-11-01 10:37:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import random
 from setuptools import setup
 
 v = "0.0.1a"
-if version.endswith(('a', 'b', 'rc')):
+if v.endswith(('a', 'b', 'rc')):
     # append version identifier based on commit count
     try:
         import subprocess
@@ -11,12 +11,12 @@ if version.endswith(('a', 'b', 'rc')):
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += out.decode('utf-8').strip()
+            v += out.decode('utf-8').strip()
         p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += '+g' + out.decode('utf-8').strip()
+            v += '+g' + out.decode('utf-8').strip()
     except Exception:
         pass
 setup(
