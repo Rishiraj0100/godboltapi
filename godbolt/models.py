0db0b300546976a1f7f95fd3dad8aefe6d144df8
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-02-04 11:33:41 +0530

    Update models.py

--- godbolt/models.py
+++ godbolt/models.py
@@ -57,9 +57,10 @@ class Route:
 
     return resp
 
-class LanguageStream(list, dict):
+class LanguageStream(list):
   def __init__(self, *args: Any, **kwargs: Any) -> None:
     super().__init__(*args, **kwargs)
+    self.__dat = {}
     asyncio.get_event_loop().create_task(self.__check_integrity())
     
   def __contains__(self, other: Union[str, LT, Any]) -> bool:
@@ -78,6 +79,22 @@ class LanguageStream(list, dict):
   def append(self, language: "Language"):
     self[language.name] = language
     super().append(language)
+
+  def remove(self, language):
+    if language in self:
+      del self[language.name if isinstance(language, Language) else language]
+    super().remove(language)
+
+  def __getattr__(self, attr, default=None):
+    if hasattr(self, attr) and not attr.startswith("_"): return eval("self."+attr)
+
+    if hasattr(self.__dat, attr) and callable(getattr(self.__dat,attr)):
+      def _(self, a, *args,**kwargs):
+        return getattr(self.__dat, attr)(a.lower(),*args,**kwargs)
+      return _
+    elif hasattr(self.__dat, attr): return getattr(self.__dat, attr)
+
+    return default
 
 class Language:
   def __init__(self, *, id: str, name: str, extensions: List[str], monaco: str) -> None:
